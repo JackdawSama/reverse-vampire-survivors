9ee825453f031b8dca43cfc9a798550b21366f58
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-01-24 19:17:24 -0500

    Project Init

---
+++ RVP/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 5f;
+    [SerializeField] Vector3 direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Attacked Player");
+        }
+    }
+}
