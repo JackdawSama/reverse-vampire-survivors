828539fb558f169a8bde884fd2cd98f0682a5123
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-03-14 23:52:49 -0400

    Implemented the framework for Observer Pattern

---
+++ RVP/Assets/Scripts/Observer Scripts/Subject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Subject : MonoBehaviour
+{
+    private List<IObserver> observers = new List<IObserver>();
+
+    public void AddObserver(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    protected void NotifyObservers()
+    {
+        observers.ForEach(observer => observer.OnNotify());
+    }
+} 
