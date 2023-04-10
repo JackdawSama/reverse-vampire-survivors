3b10a28ba1a0d688a6d2773c4c4b3d18ed2bf9a6
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-04-09 23:11:45 -0400

    Name changes

--- RVP/Assets/Scripts/CombatFIFA/ThePlayerController.cs
+++ RVP/Assets/Scripts/CombatFIFA/ThePlayerController.cs
@@ -26,12 +26,12 @@ public class ThePlayerController : MonoBehaviour
     public Transform bulletSpawn;
     public GameObject projectilePrefab;
     public GameObject damageTextPrefab;
-    public SpriteRenderer renderer;
+    public SpriteRenderer rend;
 
     void Start()
     {
         center = hero.transform.position;
-        renderer = GetComponent<SpriteRenderer>();
+        rend = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -58,13 +58,13 @@ public class ThePlayerController : MonoBehaviour
         if(Input.GetKey(moveLeft))
         {
             angle += moveSpeed * Time.deltaTime;
-            renderer.flipX = false;
+            rend.flipX = false;
 
         }
         else if(Input.GetKey(moveRight))
         {
             angle += -moveSpeed * Time.deltaTime;
-            renderer.flipX = true;
+            rend.flipX = true;
         }
 
 
