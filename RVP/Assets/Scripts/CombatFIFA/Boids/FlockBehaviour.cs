41016f50d05b00f1756dea6e40a303c2f90b4ded
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-04-12 01:13:17 -0400

    Cleaned Up project for more clarity and focus

--- RVP/Assets/Scripts/CombatFIFA/Boids/FlockBehaviour.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class FlockBehaviour : ScriptableObject
-{
-    public abstract Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock);
-}
