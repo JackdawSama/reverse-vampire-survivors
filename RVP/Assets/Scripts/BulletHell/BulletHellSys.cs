51974eb79c2c545d8ac292c94b57e46b3b2eb30a
Akshay Bharadhwaj <46255105+JackdawSama@users.noreply.github.com>
2023-04-26 22:42:57 -0400

    Setup Bull Hell States

--- RVP/Assets/Scripts/BulletHell/BulletHellSys.cs
+++ RVP/Assets/Scripts/BulletHell/BulletHellSys.cs
@@ -3,35 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletHellSys : MonoBehaviour
-{
-    public bool activate;
+{   
+    [Header("References")]
+    public TheHero hero;
+
+    [Header("Timers")]
     [SerializeField] float bulletHellTimer; 
     [SerializeField] float bulletHellCooldown;
-    [SerializeField] float oneShotTimer;
-    [SerializeField] float oneShotCooldown;
+    [SerializeField] float modeTimer;
+    [SerializeField] float modeCooldown = 3f;
 
-    public List<AttackTypes> attackTypes;
-    public GameObject[] projectilePrefab;
-
+    [Header("Emitter Data")]
     public Transform[] emitters;
-    public float emitterAngle, projectileAngle, projectileAmount;
+    public GameObject[] projectilePrefab;
+    public List<AttackTypes> attackTypes;
+    float emitterAngle, projectileAngle, projectileAmount;
 
+    [Header("Bullet Hell State")]
     public BulletHell bulletHell;
     public BulletHell bulletHellRefState;
     public enum BulletHell
     {
-        //SE - Single Emitter, DE - Double Emitter
-        //N - North, S - South, E - East, W - West
         Inactive,
-        ContinuousSE,
-        OneShotSE,
-        ContinuousDE,
-        OneShotDE,
+        ModeOne,
+        ModeTwo,
+        ModeThree,
+        ModeFour,
         Chaos
     }
+
     // Start is called before the first frame update
     void Start()
     {
+        hero = GetComponent<TheHero>();
+
         bulletHell = BulletHell.Inactive;
         SetPattern(attackTypes[0]);
     }
@@ -54,59 +59,105 @@ public class BulletHellSys : MonoBehaviour
         {
             case BulletHell.Inactive:              //Attack Switch state to allow for routine to end switch to another state
             {
-                if(activate)
+                if(!hero.shieldsActive)
                 {
                     //Do a health check and based on health percentage, set the bullet hell state
+                    if(hero.HealthPercentage() > 75f)
+                    {
+                        bulletHell = BulletHell.ModeOne;
+                    }
+                    else if(hero.HealthPercentage() <= 75f && hero.HealthPercentage() > 50f)
+                    {
+                        bulletHell = BulletHell.ModeTwo;
+                    }
+                    else if(hero.HealthPercentage() <= 50f && hero.HealthPercentage() > 25f)
+                    {
+                        bulletHell = BulletHell.ModeOne;
+                    }
+                    else if(hero.HealthPercentage() <= 25f)
+                    {
+                        bulletHell = BulletHell.Chaos;
+                    }
+
                 }
                 break;
             }
 
-            case BulletHell.OneShotSE:                 //Single Attack EmitterState
+            case BulletHell.ModeOne:                 //Single Attack EmitterState
             {
-                bulletHellRefState = BulletHell.OneShotSE;
-
-                SetPattern(attackTypes[1]);
-
-                OneShotSE();
-                emitters[0].rotation = Quaternion.Euler(emitters[0].eulerAngles.x, emitters[0].eulerAngles.y, (emitters[0].eulerAngles.z  + emitterAngle));
-
-                break;
-            }
-
-            case BulletHell.ContinuousSE:                 //Single Attack EmitterState
-            {
-                bulletHellRefState = BulletHell.ContinuousSE;
-
                 SetPattern(attackTypes[0]);
 
                 ContinuousSE();
                 emitters[0].rotation = Quaternion.Euler(emitters[0].eulerAngles.x, emitters[0].eulerAngles.y, (emitters[0].eulerAngles.z  + emitterAngle));
 
-                break;
-            }
-
-
-            case BulletHell.OneShotDE:
-            {
-                bulletHellRefState = BulletHell.OneShotDE;
-                SetPattern(attackTypes[1]);
-
-                OneShotDE();
-                emitters[1].rotation = Quaternion.Euler(emitters[1].eulerAngles.x, emitters[1].eulerAngles.y, (emitters[1].eulerAngles.z  + emitterAngle));
-                emitters[3].rotation = Quaternion.Euler(emitters[3].eulerAngles.x, emitters[3].eulerAngles.y, (emitters[3].eulerAngles.z  - emitterAngle));   
+                if(hero.HealthPercentage() <= 75f && hero.HealthPercentage() > 50f)
+                {
+                    bulletHell = BulletHell.ModeThree;
+                }
 
                 break;
             }
 
-            case BulletHell.ContinuousDE:
+            case BulletHell.ModeTwo:                 //Single Attack EmitterState
             {
-                bulletHellRefState = BulletHell.ContinuousDE;
+                modeTimer += Time.deltaTime;
                 SetPattern(attackTypes[1]);
+
+                ContinuousSE();
+                emitters[0].rotation = Quaternion.Euler(emitters[0].eulerAngles.x, emitters[0].eulerAngles.y, (emitters[0].eulerAngles.z  + emitterAngle));
+
+                if(hero.HealthPercentage() <= 25f)
+                {
+                    modeTimer = 0;
+                    bulletHell = BulletHell.Chaos;
+                }
+
+                if(modeTimer >= modeCooldown)
+                {
+                    bulletHell = BulletHell.ModeFour;
+                    modeTimer = 0;
+                }
+
+                break;
+            }
+
+
+            case BulletHell.ModeThree:
+            {
+                SetPattern(attackTypes[2]);
 
                 ContinuousDE();
                 emitters[1].rotation = Quaternion.Euler(emitters[1].eulerAngles.x, emitters[1].eulerAngles.y, (emitters[1].eulerAngles.z  + emitterAngle));
+                emitters[3].rotation = Quaternion.Euler(emitters[3].eulerAngles.x, emitters[3].eulerAngles.y, (emitters[3].eulerAngles.z  + emitterAngle));   
+
+                if(hero.HealthPercentage() <= 50f && hero.HealthPercentage() > 25f)
+                {
+                    bulletHell = BulletHell.ModeTwo;
+                }
+
+                break;
+            }
+
+            case BulletHell.ModeFour:
+            {
+                modeTimer += Time.deltaTime;
+                SetPattern(attackTypes[3]);
+
+                ContinuousDE();
+                emitters[1].rotation = Quaternion.Euler(emitters[1].eulerAngles.x, emitters[1].eulerAngles.y, (emitters[1].eulerAngles.z  - emitterAngle));
                 emitters[3].rotation = Quaternion.Euler(emitters[3].eulerAngles.x, emitters[3].eulerAngles.y, (emitters[3].eulerAngles.z  - emitterAngle));   
 
+                if(hero.HealthPercentage() <= 25f)
+                {
+                    bulletHell = BulletHell.Chaos;
+                }
+
+                if(modeTimer >= modeCooldown)
+                {
+                    bulletHell = BulletHell.ModeFour;
+                    modeTimer = 0;
+                }
+                
                 break;
             }
 
@@ -116,7 +167,7 @@ public class BulletHellSys : MonoBehaviour
                 SetPattern(attackTypes[4]);
 
                 Chaos();
-                emitters[0].rotation = Quaternion.Euler(emitters[0].eulerAngles.x, emitters[0].eulerAngles.y, (emitters[0].eulerAngles.z  + emitterAngle));
+                emitters[0].rotation = Quaternion.Euler(emitters[0].eulerAngles.x, emitters[0].eulerAngles.y, (emitters[0].eulerAngles.z));
                 emitters[1].rotation = Quaternion.Euler(emitters[1].eulerAngles.x, emitters[1].eulerAngles.y, (emitters[1].eulerAngles.z  + emitterAngle));
                 emitters[3].rotation = Quaternion.Euler(emitters[3].eulerAngles.x, emitters[3].eulerAngles.y, (emitters[3].eulerAngles.z  - emitterAngle));  
 
@@ -126,21 +177,6 @@ public class BulletHellSys : MonoBehaviour
             default:
                 break;
         }
-    }
-
-    private void OneShotSE()
-    {
-        // new better way
-        for (int i = 0; i < projectileAmount; i++)
-        {
-            //generating on instance
-            Transform bullet = Instantiate(projectilePrefab[1], emitters[0].position, emitters[0].rotation).transform;           //saves the Transform reference
-            Quaternion rot = Quaternion.Euler(0, 0, emitters[0].eulerAngles.z + (projectileAngle * i));                          //updates the angle between this and the next bullet
-            bullet.transform.rotation = rot;                                                                                     //changes the emitter's current rotation
-        }
-
-        // reset the attack timer
-        bulletHellTimer = 0f;
     }
 
     private void ContinuousSE()
@@ -155,24 +191,6 @@ public class BulletHellSys : MonoBehaviour
         }
 
         // reset the attack timer
-        bulletHellTimer = 0f;
-    }
-
-
-    private void OneShotDE()
-    {
-        //BH Attack that uses North and South Emitter - Sine and Cos Projectiles //TODO - Set Projectiles to be Sine and Cos
-        for (int i = 0; i < projectileAmount; i++)
-        {
-            Transform bulletOne = Instantiate(projectilePrefab[1], emitters[2].position, emitters[2].rotation).transform;
-            Quaternion rotOne = Quaternion.Euler(0, 0, emitters[2].eulerAngles.z + (projectileAngle * i));
-            bulletOne.transform.rotation = rotOne;
-
-            Transform bulletTwo = Instantiate(projectilePrefab[2], emitters[4].position, emitters[4].rotation).transform;
-            Quaternion rotTwo = Quaternion.Euler(0, 0, emitters[4].eulerAngles.z + (projectileAngle * i));
-            bulletTwo.transform.rotation = rotTwo; 
-        }
-
         bulletHellTimer = 0f;
     }
 
